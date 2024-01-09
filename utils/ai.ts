fe195a8bdfaeafe43e1ae131f06a0fab4fc27c82
thenishantgiri <nishantkrgiri@gmail.com>
2024-01-09 15:37:14 +0530

    Understanding AI & Prompt Engineering: Structured Output with Zod

--- utils/ai.ts
+++ utils/ai.ts
@@ -1,4 +1,31 @@
+import { z } from 'zod';
 import { OpenAI } from '@langchain/openai';
+import { StructuredOutputParser } from 'langchain/output_parsers';
+
+const parser = StructuredOutputParser.fromZodSchema(
+  z.object({
+    mood: z
+      .string()
+      .describe('the mood of the person who wrote the journal entry.'),
+    subject: z.string().describe('the subject of the journal entry.'),
+    negative: z
+      .boolean()
+      .describe(
+        'is the journal entry negative? (i.e. does it contain negative emotions?).'
+      ),
+    summary: z.string().describe('quick summary of the entire entry.'),
+    color: z
+      .string()
+      .describe(
+        'a hexidecimal color code that represents the mood of the entry. Example #0101fe for blue representing happiness.'
+      ),
+    sentimentScore: z
+      .number()
+      .describe(
+        'sentiment of the text and rated on a scale from -10 to 10, where -10 is extremely negative, 0 is neutral, and 10 is extremely positive.'
+      ),
+  })
+);
 
 export const analyze = async (prompt: any) => {
   /**
